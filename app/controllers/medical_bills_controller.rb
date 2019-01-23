bdf09d7e6254523de843d6cc1b14a0cbfd3592dd
lime1024 <lime1024@gmail.com>
2019-01-23 23:45:56 +0900

    Add rubyXL gem and output of excel
    rubyXL gem の追加とエクセル出力機能を追加

--- app/controllers/medical_bills_controller.rb
+++ app/controllers/medical_bills_controller.rb
@@ -33,6 +33,41 @@ class MedicalBillsController < ApplicationController
     redirect_to medical_bills_url, notice: "#{medical_bill.name}の#{medical_bill.payee}を登録しました。"
   end
 
+  def output
+    @medical_bills = MedicalBill.all
+    @total_cos = MedicalBill.sum(:cost)
+
+    @workbook = RubyXL::Parser.parse(Rails.root.join("template", "template.xlsx"))
+    @sheet = @workbook.first
+
+    @sheet[2][2].change_contents(@total_cost) # 合計金額
+    
+    num = 8
+    @medical_bills.each.with_index(1){|medical_bill, index|
+      if medical_bill.classification == "治療費"
+        @sheet[num][1].change_contents(medical_bill.name) # 名前
+        @sheet[num][2].change_contents(medical_bill.payee) # 支払先
+        @sheet[num][3].change_contents("該当する") # 区分
+        @sheet[num][7].change_contents(medical_bill.cost) # 金額
+      elsif medical_bill.classification == "医薬品費"
+        @sheet[num][1].change_contents(medical_bill.name) # 名前
+        @sheet[num][2].change_contents(medical_bill.payee) # 支払先
+        @sheet[num][4].change_contents("該当する") # 区分
+        @sheet[num][7].change_contents(medical_bill.cost) # 金額
+      elsif medical_bill.classification == "交通費"
+        @sheet[num][1].change_contents(medical_bill.name) # 名前
+        @sheet[num][2].change_contents(medical_bill.payee) # 支払先
+        @sheet[num][5].change_contents("該当する") # 区分
+        @sheet[num][7].change_contents(medical_bill.cost) # 金額        
+      end
+      num += 1
+    }
+
+    filename = SecureRandom.urlsafe_base64(8)
+    @workbook.write(Rails.root.join("tmp", "#{filename}.xlsx"))
+    send_file(Rails.root.join("tmp", "#{filename}.xlsx"))
+  end
+
   private
 
   def medical_bill_params
