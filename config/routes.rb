059b655e51120ee82f7651a772b4742f26e6bfe1
lime1024 <lime1024@gmail.com>
2019-01-26 18:19:30 +0900

    Edit routes
    routes の書き方を collection にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'medical_bills#index'
-  get "/medical_bills/output", to: 'medical_bills#output'
-  resources :medical_bills
+  resources :medical_bills do
+    get 'output', on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
