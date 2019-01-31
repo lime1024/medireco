e17ea3378662deee49706cf862274bf8fad1e0a0
lime1024 <lime1024@gmail.com>
2019-01-31 17:44:34 +0900

    年の検索機能を追加

--- app/models/medical_bill.rb
+++ app/models/medical_bill.rb
@@ -1,2 +1,9 @@
 class MedicalBill < ApplicationRecord
+  def self.search(search)
+    if search
+      MedicalBill.where("day BETWEEN ? AND ?", "#{search}-01-01", "#{search}-03-31")
+    else
+      MedicalBill.all
+    end
+  end
 end
