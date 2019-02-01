619dcfffaaf117117461d9c7ac3ff6bce8376be0
lime1024 <lime1024@gmail.com>
2019-02-01 20:42:02 +0900

    去年のデータのエクセル出力機能を実装

--- app/controllers/medical_bills_controller.rb
+++ app/controllers/medical_bills_controller.rb
@@ -72,13 +72,53 @@ class MedicalBillsController < ApplicationController
     send_file(Rails.root.join("tmp", "#{filename}.xlsx"))
   end
 
+  def last_year_output
+    this_year = Date.today.year
+    last_year = this_year - 1
+    @medical_bills_last_year_output = MedicalBill.where("day BETWEEN ? AND ?", "#{last_year}-01-01", "#{last_year}-03-31")
+
+    @total_cost = @medical_bills_last_year_output.sum(:cost)
+
+    @workbook = RubyXL::Parser.parse(Rails.root.join("template", "template.xlsx"))
+    @sheet = @workbook.first
+
+    @sheet[2][2].change_contents(@total_cost) # 合計金額
+
+    num = 8
+    @medical_bills_last_year_output.each.with_index(1){ |medical_bill, index|
+      if medical_bill.classification == "治療費"
+        @sheet[num][0].change_contents(index) # No.
+        @sheet[num][1].change_contents(medical_bill.name) # 名前
+        @sheet[num][2].change_contents(medical_bill.payee) # 支払先
+        @sheet[num][3].change_contents("該当する") # 区分
+        @sheet[num][7].change_contents(medical_bill.cost) # 金額
+      elsif medical_bill.classification == "医薬品費"
+        @sheet[num][0].change_contents(index) # No.
+        @sheet[num][1].change_contents(medical_bill.name) # 名前
+        @sheet[num][2].change_contents(medical_bill.payee) # 支払先
+        @sheet[num][4].change_contents("該当する") # 区分
+        @sheet[num][7].change_contents(medical_bill.cost) # 金額
+      elsif medical_bill.classification == "交通費"
+        @sheet[num][0].change_contents(index) # No.
+        @sheet[num][1].change_contents(medical_bill.name) # 名前
+        @sheet[num][2].change_contents(medical_bill.payee) # 支払先
+        @sheet[num][6].change_contents("該当する") # 区分
+        @sheet[num][7].change_contents(medical_bill.cost) # 金額        
+      end
+      num += 1
+    }
+
+    filename = SecureRandom.urlsafe_base64(8)
+    @workbook.write(Rails.root.join("tmp", "#{filename}.xlsx"))
+    send_file(Rails.root.join("tmp", "#{filename}.xlsx"))
+  end
+
   def last_year_delete
     this_year = Date.today.year
     last_year = this_year - 1
     @medical_bills_delete = MedicalBill.where("day BETWEEN ? AND ?", "#{last_year}-01-01", "#{last_year}-03-31").destroy_all
     redirect_to root_url
   end
-
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :medical_bills do
     get 'output', on: :collection
     delete 'last_year_delete', on: :collection
+    get 'last_year_output', on: :collection
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
