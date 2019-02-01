17da18c7e485cd96bf834e683e8b68cd41fab416
lime1024 <lime1024@gmail.com>
2019-02-01 17:51:56 +0900

    去年の記録の全削除機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'medical_bills#index'
   resources :medical_bills do
     get 'output', on: :collection
+    delete 'last_year_delete', on: :collection
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
