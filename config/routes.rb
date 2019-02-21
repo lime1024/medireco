309ac19769082548915d437168caa6d657fc97b4
lime1024 <lime1024@gmail.com>
2019-02-21 13:51:39 +0900

    ログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   root to: 'medical_bills#index'
   resources :medical_bills do
     get 'output', on: :collection
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_084750) do
+ActiveRecord::Schema.define(version: 2019_02_21_043846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_02_19_084750) do
     t.integer "cost", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_medical_bills_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
