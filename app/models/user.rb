b3ac919a05f64e4986d4f8f89909c241f4e0f490
lime1024 <lime1024@gmail.com>
2019-02-27 11:46:57 +0900

    ユーザを削除したら、関連する DB も削除する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :medical_bills
-  has_many :family_members
+  has_many :medical_bills, dependent: :destroy
+  has_many :family_members, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
