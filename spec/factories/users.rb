82218d8b296905f2a4ade63fb76e0e8ce0634c0b
lime1024 <lime1024@gmail.com>
2019-03-21 22:31:32 +0900

    スペースを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { 'まぐろ' }
-    email {'maguro@example.com'}
-    password {'password'}
+    email { 'maguro@example.com' }
+    password { 'password' }
   end
 end
