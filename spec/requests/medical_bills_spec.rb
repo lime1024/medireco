820750607546b4130e3ddc391bd91b3a688f1515
lime1024 <lime1024@gmail.com>
2020-03-01 00:29:48 +0900

    予告通りmedical_billsは置き換えたぞ

--- spec/requests/medical_bills_spec.rb
+++ spec/requests/medical_bills_spec.rb
@@ -1,24 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe '医療費の登録', type: :request do
-  let!(:user) { FactoryBot.create(:user, name: 'まぐろ', email: 'maguro@example.com', password: 'password') }
-  let!(:payee) { FactoryBot.create(:payee, name: 'おさかな病院', user: user) }
+  let(:user) { FactoryBot.create(:user) }
+  let!(:payee) { FactoryBot.create(:payee, user: user) }
+  let!(:family_member) { FactoryBot.create(:family_member, user: user) }
 
-  context 'ログインしているとき' do
+  context 'ログインしている場合' do
     before do
       login(user)
     end
 
-    it '医療費の登録ができる' do
+    it '医療費の登録ができること' do
+      expect {
+        post medical_bills_path, params: {
+          medical_bill: {
+            day: '2011-01-11',
+            family_member_id: user.family_members.first,
+            payee_id: payee.id,
+            classification: '治療費',
+            cost: '111111',
+            user_id: user.id,
+            family_member_id: family_member.id,
+            payee_id: payee.id
+          }
+        }
+      }.to change(MedicalBill, :count).by(1)
+    end
+
+    xit do
+      post medical_bills_path
+      expect(response).to redirect_to(medical_bills_path)
+    end
+
+    it '医療費が編集できること' do
+      medical_bill = FactoryBot.create(:medical_bill, user: user, cost: 111111)
+      expect {
+        patch medical_bill_path(medical_bill.id), params: {
+          medical_bill: {
+            cost: '123456'
+          }
+        }
+      }.to change{ medical_bill.reload.cost }.from(111111).to(123456)
+    end
+
+    it '医療費を削除できること' do
+      medical_bill = FactoryBot.create(:medical_bill, user: user)
+      expect {
+        delete medical_bill_path(medical_bill.id)
+      }.to change(MedicalBill, :count).by(-1)
+    end
+  end
+
+  context 'ログインしていない場合' do
+    it do
       post medical_bills_path, params: {
         medical_bill: {
           day: '2011-01-11',
           family_member_id: user.family_members.first,
           payee_id: payee.id,
           classification: '治療費',
-          cost: '111111'
+          cost: '111111',
+          user_id: user.id,
+          family_member_id: family_member.id,
+          payee_id: payee.id
         }
       }
+      expect(response).to redirect_to(login_path)
     end
   end
 end
