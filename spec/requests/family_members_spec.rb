75ec79d6cda562291c2bf9fc23d25f833c8cc42c
lime1024 <lime1024@gmail.com>
2020-05-09 22:21:48 +0900

    family_members_spec を request spec に移行

--- spec/requests/family_members_spec.rb
+++ spec/requests/family_members_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe '家族の登録', type: :request do
-  let!(:user) { FactoryBot.create(:user, name: 'まぐろ', email: 'maguro@example.com', password: 'password') }
+  let(:user) { FactoryBot.create(:user, name: 'まぐろ', email: 'maguro@example.com', password: 'password') }
   let!(:family_member) { FactoryBot.create(:family_member, name: 'しゃけ', user: user) }
 
-  context '家族が登録されているとき' do
-    it '家族を変更できる' do
+  context 'ログインしている場合' do
+    before do
       login(user)
+    end
+
+    it '家族を登録できる' do
+      expect {
+        post user_family_members_path(user), params: {
+          family_member: {
+            name: 'かんぱち'
+          }
+        }
+      }.to change(FamilyMember, :count).by(1)
+    end
+
+    it '家族を変更できる' do
       patch user_family_member_path(user, family_member), params: {
         family_member: {
           name: 'のどぐろ'
@@ -14,6 +27,23 @@ RSpec.describe '家族の登録', type: :request do
       }
       expect(response).to redirect_to(user_family_members_path(user))
       expect(FamilyMember.find(family_member.id).name).to eq 'のどぐろ'
+    end
+
+    it '家族を削除できる' do
+      expect {
+        delete user_family_member_path(user, family_member)
+      }.to change(FamilyMember, :count).by(-1)
+    end
+  end
+
+  context 'ログインしていない場合' do
+    it do
+      post user_family_members_path(user), params: {
+        family_member: {
+          name: 'めばち'
+        }
+      }
+      expect(response).to redirect_to(login_path)
     end
   end
 end
