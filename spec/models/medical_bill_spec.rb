7b8ea627625b9542f26c3aca4d971dcf2a5ef73b
lime1024 <lime1024@gmail.com>
2020-05-14 18:44:46 +0900

    model spec のリファクタリング

--- spec/models/medical_bill_spec.rb
+++ spec/models/medical_bill_spec.rb
@@ -1,67 +1,67 @@
 require 'rails_helper'
 
-describe 'medical_bill', type: :model do
-  context 'select_year 同一年が複数あったとき' do
-    before do
-      FactoryBot.create(:medical_bill, day: Date.new(2019, 01, 01))
-      FactoryBot.create(:medical_bill, day: Date.new(2019, 02, 02))
-      FactoryBot.create(:medical_bill, day: Date.new(2019, 03, 03))
+RSpec.describe MedicalBill, type: :model do
+  describe '.select_year' do
+    context '同一年が複数ある場合' do
+      it '返り値が重複しないこと' do
+        FactoryBot.create(:medical_bill, day: Date.new(2019, 01, 01))
+        FactoryBot.create(:medical_bill, day: Date.new(2019, 02, 02))
+        FactoryBot.create(:medical_bill, day: Date.new(2019, 03, 03))
+
+        expect(described_class.select_year).to eq [2019]
+      end
     end
 
-    it '返り値が重複しない' do
-      expect(MedicalBill.select_year).to eq [2019]
-    end
-  end
+    context '異なる年が複数ある場合' do
+      it '全ての異なる年が返ってくること' do
+        FactoryBot.create(:medical_bill, day: Date.new(2019, 01, 01))
+        FactoryBot.create(:medical_bill, day: Date.new(2017, 03, 03))
+        FactoryBot.create(:medical_bill, day: Date.new(2018, 02, 02))
 
-  context 'select_year 異なる年が複数あったとき' do
-    before do
-      FactoryBot.create(:medical_bill, day: Date.new(2019, 01, 01))
-      FactoryBot.create(:medical_bill, day: Date.new(2018, 02, 02))
-      FactoryBot.create(:medical_bill, day: Date.new(2017, 03, 03))
-    end
-
-    it '全ての異なる年が返ってくる' do
-      expect(MedicalBill.select_year).to eq [2019, 2018, 2017]
+        expect(described_class.select_year).to match_array([2019, 2018, 2017])
+      end
     end
   end
 
-  context 'summarized_output 同一支払先に複数回支払ったとき' do
-    before do
-      FactoryBot.create(:medical_bill, day: Date.new(2019, 01, 01), cost: 300)
-      FactoryBot.create(:medical_bill, day: Date.new(2018, 02, 02), cost: 500)
-      FactoryBot.create(:medical_bill, day: Date.new(2017, 03, 03), cost: 200)
-    end
+  describe '.summarized_output' do
+    context '同一支払先に複数回支払った場合' do
+      it '支払った金額が合算されること' do
+        FactoryBot.create(:medical_bill, day: Date.new(2019, 01, 01), cost: 300)
+        FactoryBot.create(:medical_bill, day: Date.new(2018, 02, 02), cost: 500)
+        FactoryBot.create(:medical_bill, day: Date.new(2017, 03, 03), cost: 200)
 
-    it '支払った金額が合算される' do
-      expect(MedicalBill.summarized_output).to eq ["しゃけ", "おさかな病院", "治療費"]=>1000
-    end
-  end
-
-  context 'this_year_total_cost 今年に支払いがあったとき' do
-    before do
-      FactoryBot.create(:medical_bill, day: Date.today, cost: 300)
-      FactoryBot.create(:medical_bill, day: Date.today, cost: 500)
-      FactoryBot.create(:medical_bill, day: Date.new(2017, 03, 03), cost: 200)
-    end
-
-    it '今年の支払額が合算される' do
-      expect(MedicalBill.this_year_total_cost).to eq 800
+        expect(described_class.summarized_output.values).to eq [1000]
+      end
     end
   end
 
-  context '医療費登録をしたとき' do
-    it 'can_not_set_future_date 未来日なら登録できない' do
-      medical_bill = MedicalBill.new(day: Date.tomorrow, classification: "治療費", cost: 100)
+  describe '.this_year_total_cost' do
+    context '今年に支払いがあった場合' do
+      it '支払額が合算されること' do
+        FactoryBot.create(:medical_bill, day: Date.today, cost: 300)
+        FactoryBot.create(:medical_bill, day: Date.today, cost: 500)
+        FactoryBot.create(:medical_bill, day: Date.new(2017, 03, 03), cost: 200)
+
+        expect(described_class.this_year_total_cost).to eq 800
+      end
+    end
+  end
+
+  describe '#can_not_set_future_date' do
+    it '未来日なら登録できないこと' do
+      medical_bill = described_class.new(day: Date.tomorrow, classification: "治療費", cost: 100)
+      expect(medical_bill).to_not be_valid
+    end
+  end
+
+  describe '#can_not_set_below_zero_yen' do
+    it '0 円なら登録できないこと' do
+      medical_bill = described_class.new(day: Date.new(2019, 01, 01), classification: "治療費", cost: 0)
       expect(medical_bill).to_not be_valid
     end
 
-    it 'can_not_set_below_zero_yen 0 円なら登録できない' do
-      medical_bill = MedicalBill.new(day: Date.new(2019, 01, 01), classification: "治療費", cost: 0)
-      expect(medical_bill).to_not be_valid
-    end
-
-    it 'can_not_set_below_zero_yen 0 円以下なら登録できない' do
-      medical_bill = MedicalBill.new(day: Date.new(2019, 01, 01), classification: "治療費", cost: -100)
+    it '0 円以下なら登録できないこと' do
+      medical_bill = described_class.new(day: Date.new(2019, 01, 01), classification: "治療費", cost: -100)
       expect(medical_bill).to_not be_valid
     end
   end
