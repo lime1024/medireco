75043ad9314ef1617e8ac4c2b66d8a9b3df0f693
lime1024 <lime1024@gmail.com>
2022-03-08 21:59:14 +0900

    合計金額はエクセルの計算式に任せるから

--- app/models/medical_bill_output.rb
+++ app/models/medical_bill_output.rb
@@ -3,15 +3,12 @@ require 'rubyXL/convenience_methods'
 class MedicalBillOutput
   def initialize(user:, year:)
     @medical_bills = user.medical_bills.search(year).summarized_output
-    @total_cost = user.medical_bills.sum(:cost)
   end
 
   def as_xlsx
     workbook = RubyXL::Parser.parse(Rails.root.join("template", "template.xlsx"))
     sheet = workbook.first
 
-    sheet[2][2].change_contents(@total_cost) # 合計金額
-    
     num = 8
 
     @medical_bills.each.with_index(1) do |medical_bill, index|
